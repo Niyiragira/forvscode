16a03cdb3729bbebfd85b47e1e250925287feaa0
Niyiragira Gerard <niyiragiragerard@gmail.com>
2020-08-28 06:29:31 +0300

    ft-fetch-from-cloud-mongodb-RLrwcC1S

--- fetch.js
+++ fetch.js
@@ -1,19 +1,32 @@
 
-const getBtn=document.getElementById("getBtn");
-
 //GETTING THE ARTICLES
-const getData= () =>{
-  try {
-    fetch('http://localhost:3000/blogs'
-    )
-  .then(response => response.json())
-  .then(json => console.log(json))
-  } catch (error) {
-    console.log(error)
-  }
-} 
-getBtn.addEventListener("click",getData);
+const container = document.querySelector('#div2')
 
+const renderPost = (doc) => {
+    const post = document.createElement('div')
+    post.setAttribute('class', 'post')
+
+    const title = document.createElement('h1')
+    title.setAttribute('class', 'post-title')
+    title.textContent = doc.title
+
+    const content = document.createElement('p')
+    content.setAttribute('class', 'post-content')
+    content.textContent = doc.content
+
+    post.appendChild(title)
+    post.appendChild(content)
+
+    container.appendChild(post)
+}
+
+fetch('http://localhost:3000/blogs', {
+    method: 'GET'
+}).then(res=>res.json())
+.then(dt=>{
+    console.log(dt)
+    dt.forEach(article => renderPost(article))
+})
 
 //Posting articles
 
@@ -21,19 +34,23 @@ getBtn.addEventListener("click",getData);
 const myForm=document.getElementById("postBtn");
 myForm.addEventListener("click",function(e){
   e.preventDefault();
-let title=document.getElementById("title");
-let content=document.getElementById("content");
+let title=document.querySelector("#title").value;
+let content=document.querySelector("#content").value;
+
   
   fetch("http://localhost:3000/blogs",{
     cors:"no-cors",
     method:"POST",
+    headers:{
+      "Content-type":"application/json; charset=UTF-8"
+    },
     body:JSON.stringify({
       title:title,
       content:content
-    })
+  })
   })
   .then(response=>{
-    console.log(response);
+    return response.json();
   })
 })
 
